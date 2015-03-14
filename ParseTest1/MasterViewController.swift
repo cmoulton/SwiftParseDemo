6fefd35ac87adfe8a3805a6aa3cee6bdd7a4e593
Christina Moulton <christina@teakmobile.com>
2015-03-14 12:58:17 -0400

    Filled in get call in master view controller

---
+++ ParseTest1/MasterViewController.swift
@@ -0,0 +1,116 @@
+//
+//  MasterViewController.swift
+//  ParseTest1
+//
+//  Created by Christina Moulton on 2015-03-09.
+//  Copyright (c) 2015 Teak Mobile Inc. All rights reserved.
+//
+
+import UIKit
+
+class MasterViewController: UITableViewController {
+
+  var detailViewController: DetailViewController? = nil
+  var spots = NSMutableArray()
+
+  override func awakeFromNib() {
+    super.awakeFromNib()
+    if UIDevice.currentDevice().userInterfaceIdiom == .Pad {
+        self.clearsSelectionOnViewWillAppear = false
+        self.preferredContentSize = CGSize(width: 320.0, height: 600.0)
+    }
+  }
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+    self.navigationItem.leftBarButtonItem = self.editButtonItem()
+
+    let addButton = UIBarButtonItem(barButtonSystemItem: .Add, target: self, action: "insertNewObject:")
+    self.navigationItem.rightBarButtonItem = addButton
+    if let split = self.splitViewController {
+        let controllers = split.viewControllers
+        self.detailViewController = controllers[controllers.count-1].topViewController as? DetailViewController
+    }
+  }
+  
+  override func viewWillAppear(animated: Bool) {
+    super.viewWillAppear(animated)
+    self.getSpots()
+  }
+  
+  // MARK: API
+  func getSpots()
+  {
+    APIController.getSpots({ (fetchedSpots, error) in
+      if error != nil
+      {
+        // TODO: improved error handling
+        var alert = UIAlertController(title: "Error", message: "Could not load spots :( \nError message:\n \(error!.localizedDescription)", preferredStyle: UIAlertControllerStyle.Alert)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
+        self.presentViewController(alert, animated: true, completion: nil)
+      }
+      if let newSpots = fetchedSpots
+      {
+        self.spots.addObjectsFromArray(fetchedSpots!)
+      }
+
+      self.tableView?.reloadData()
+    })
+  }
+
+  func insertNewObject(sender: AnyObject) {
+    spots.insertObject(NSDate(), atIndex: 0)
+    let indexPath = NSIndexPath(forRow: 0, inSection: 0)
+    self.tableView.insertRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
+  }
+
+  // MARK: - Segues
+
+  override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+    if segue.identifier == "showDetail" {
+        if let indexPath = self.tableView.indexPathForSelectedRow() {
+            let object = spots[indexPath.row] as NSDate
+            let controller = (segue.destinationViewController as UINavigationController).topViewController as DetailViewController
+            controller.detailItem = object
+            controller.navigationItem.leftBarButtonItem = self.splitViewController?.displayModeButtonItem()
+            controller.navigationItem.leftItemsSupplementBackButton = true
+        }
+    }
+  }
+
+  // MARK: - Table View
+
+  override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+    return 1
+  }
+
+  override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return spots.count
+  }
+
+  override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as UITableViewCell
+
+    let object = spots[indexPath.row] as NSDate
+    cell.textLabel!.text = object.description
+    return cell
+  }
+
+  override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+    // Return false if you do not want the specified item to be editable.
+    return true
+  }
+
+  override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+    if editingStyle == .Delete {
+        spots.removeObjectAtIndex(indexPath.row)
+        tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+    } else if editingStyle == .Insert {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
+    }
+  }
+
+
+}
+
