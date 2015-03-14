1b16b6865a69358cc78617c21f6608f929170b63
Christina Moulton <christina@teakmobile.com>
2015-03-14 12:57:13 -0400

    Spot class properties & initializer

---
+++ ParseTest1/Spot.swift
@@ -0,0 +1,23 @@
+//
+//  Spot.swift
+//  ParseTest1
+//
+//  Created by Christina Moulton on 2015-03-14.
+//  Copyright (c) 2015 Teak Mobile Inc. All rights reserved.
+//
+
+import Foundation
+import MapKit
+
+class Spot {
+  let name: String
+  let location: CLLocationCoordinate2D
+  let id: Int?
+  
+  required init(aName: String, aLat: Double, aLong: Double, anId: Int?)
+  {
+    name = aName
+    location = CLLocationCoordinate2D(latitude: aLat, longitude: aLong)
+    id = anId
+  }
+}
