778f37fd902f6a8e6fd08bd1e59b7f4ffdeb6d0e
Christina Moulton <christina@teakmobile.com>
2015-03-18 11:52:47 -0400

    Added login VC

---
+++ ParseTest1/LoginViewController.swift
@@ -0,0 +1,9 @@
+//
+//  LoginViewController.swift
+//  ParseTest1
+//
+//  Created by Christina Moulton on 2015-03-18.
+//  Copyright (c) 2015 Teak Mobile Inc. All rights reserved.
+//
+
+import Foundation
